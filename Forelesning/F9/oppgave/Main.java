448e9f1707efdbcab95d40a0d1a936b184fd008d
Oleandertengesdal <oleander.tengesdal@icloud.com>
2024-10-17 09:06:28 +0200

    Create Main.java

---
+++ Forelesning/F9/oppgave/Main.java
@@ -0,0 +1,9 @@
+package Forelesning.F9.oppgave;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.start();
+    }
+}
