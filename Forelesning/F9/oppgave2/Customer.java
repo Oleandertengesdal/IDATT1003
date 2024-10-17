343f4240f02734bda9753ae2a180e5e9f683529d
Oleandertengesdal <oleander.tengesdal@icloud.com>
2024-10-17 09:06:35 +0200

    Create Customer.java

---
+++ Forelesning/F9/oppgave2/Customer.java
@@ -0,0 +1,15 @@
+package Forelesning.F9.oppgave2;
+
+public class Customer {
+
+    private String username;
+
+    private String password;
+
+    public boolean login() {
+
+        // TODO: call to liveBank
+
+        return false;
+    }
+}
